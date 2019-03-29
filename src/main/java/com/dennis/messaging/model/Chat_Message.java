4049a732ce9164a7b8d4f3b9e529b40e3c18338a
Dennis Muturia <dmuturia2922@gmail.com>
2019-03-29 10:32:34 +0300

    Created a model that will handle the messagepayload that will be exchanged between clients and the server.

---
+++ src/main/java/com/dennis/messaging/model/Chat_Message.java
@@ -0,0 +1,32 @@
+package com.dennis.messaging.model;
+
+import java.awt.*;
+
+public class Chat_Message {
+    private MessageType type;
+    private String content;
+    private String sender;
+
+    public enum MessageType{
+        CHAT, JOIN, LEAVE
+    }
+
+    public MessageType getType(){
+        return type;
+    }
+    public void setType(MessageType type){
+        this.type = type;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+}
