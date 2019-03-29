705dbe394a403bb3055eee84298e656a3f30a8ef
Dennis Muturia <dmuturia2922@gmail.com>
2019-03-29 12:22:19 +0300

    Incorporated RabbiMQ

--- src/main/java/com/dennis/messaging/config/WebSocketConfig.java
+++ src/main/java/com/dennis/messaging/config/WebSocketConfig.java
@@ -14,6 +14,6 @@ public class WebSocketConfig  implements  WebSocketMessageBrokerConfigurer{
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry){
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");
+        registry.enableStompBrokerRelay("/topic").setRelayHost("localhost").setRelayPort(5672).setClientLogin("guest").setClientPasscode("Guest");
     }
 }
